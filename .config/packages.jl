a8c31fb2ecdc84b748abf6a78ffd65caa0dcb6fd
ronaldo <vieira.lobato@gmail.com>
2021-08-21 08:28:13 -0500

    add my julia packages

---
+++ .config/packages.jl
@@ -0,0 +1,10 @@
+import Pkg # import the package manager
+Pkg.add("DifferentialEquations");
+Pkg.add("PyPlot");
+Pkg.add("DelimitedFiles");
+Pkg.add("Dierckx");
+Pkg.add("LaTeXStrings");
+Pkg.add("Distributed");
+Pkg.add("DataFrames");
+Pkg.add("CSV");
+Pkg.add("Plots")
