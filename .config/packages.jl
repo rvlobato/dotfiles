7cc57f8fb9a5b94065e289a1484b29e37e329d57
ronaldo <vieira.lobato@gmail.com>
2022-04-17 19:23:32 -0500

    some ups

--- .config/packages.jl
+++ .config/packages.jl
@@ -1,27 +1,27 @@
 import Pkg
-Pkg.add("AlphaZero") #Deepmind's AlphaZero algorithm
-Pkg.add("Arrow") #implementation of the apache arrow memory
-Pkg.add("Clustering") #Methods for data clustering and evaluation of clustering quality
+Pkg.add("AlphaZero") # Deepmind's AlphaZero algorithm
+Pkg.add("Arrow") # implementation of the apache arrow memory
 Pkg.add("CSV") #A fast, flexible delimited file reader/writer for Julia.
 Pkg.add("CUDA") # CUDA programming in Julia
-Pkg.add("DataFrames") #Tools for working with tabular data in Julia.
+Pkg.add("Clustering") # Methods for data clustering and evaluation of clustering quality
+Pkg.add("DataFrames") # Tools for working with tabular data in Julia.
 Pkg.add("DelimitedFiles")
 Pkg.add("Dierckx")
 Pkg.add("DifferentialEquations")
 Pkg.add("Distributed")
 Pkg.add("Distributions")
 Pkg.add("Flux")
-Pkg.add("Gadfly")
-Pkg.add("GLMakie") #High level plotting on the GPU.
-Pkg.add("IJulia")
+Pkg.add("Gadfly") # Gadfly is a plotting and data visualization system
+Pkg.add("GLMakie") # High level plotting on the GPU.
+Pkg.add("IJulia") # Julia kernel for Jypyter
 Pkg.add("Knet")
 Pkg.add("LanguageServer")
 Pkg.add("LaTeXStrings")
-Pkg.add("LoopVectorization") #macro(s) for vectorizing loops
+Pkg.add("LoopVectorization") # macro(s) for vectorizing loops
 Pkg.add("ModelingToolkit")
-#Pkg.add("NMFk") #Nonnegative Matrix Factorization + k-means clustering and physics constraints
-#Pkg.add("NTFk") #Nonnegative Tensor Factorization using k-means clustering and physics constraints
-#Pkg.add("NTNk") #Unsupervised Machine Learning: Nonnegative Tensor Networks + k-means clustering
+#Pkg.add("NMFk") # Nonnegative Matrix Factorization + k-means clustering and physics constraints
+#Pkg.add("NTFk") # Nonnegative Tensor Factorization using k-means clustering and physics constraints
+#Pkg.add("NTNk") # Unsupervised Machine Learning: Nonnegative Tensor Networks + k-means clustering
 Pkg.add("ObjectDetector")
 Pkg.add("Plots")
 Pkg.add("Pluto")
