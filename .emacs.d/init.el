6bbc82fbe35aa5b7d88d45a81beed9c6bb5b70f3
R. V. Lobato <rvlobato@pm.me>
2023-02-12 20:10:01 -0500

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,10 +9,12 @@
 (require 'package)
 
 ;; Internet repositories for new packages.
-(setq package-archives '(("melpa"     . "https://melpa.org/packages/")
+(setq package-archives '(
+			 ("melpa" . "https://melpa.org/packages/")
 			 ("gnu" . "https://elpa.gnu.org/packages/")
 			 ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-			 ))
+			 )
+      )
 
 ;; Activate all the packages (in particular autoloads)
 (package-initialize)
