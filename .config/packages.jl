997959ab134b3c026b22f62ab6c5bae12a4d712c
R. V. Lobato <rvlobato@pm.me>
2023-10-02 21:03:19 -0300

    Add new libraries

--- .config/packages.jl
+++ .config/packages.jl
@@ -1,6 +1,7 @@
 import Pkg
 Pkg.add("AlphaZero") # Deepmind's AlphaZero algorithm
 Pkg.add("Arrow") # implementation of the apache arrow memory
+Pkg.add("ComponentArrays") # Arrays with arbitrarily nested named components.
 Pkg.add("CSV") #A fast, flexible delimited file reader/writer for Julia.
 Pkg.add("CUDA") # CUDA programming in Julia
 Pkg.add("Clustering") # Methods for data clustering and evaluation of clustering quality
@@ -16,19 +17,27 @@ Pkg.add("GLMakie") # High level plotting on the GPU.
 Pkg.add("IJulia") # Julia kernel for Jypyter
 Pkg.add("Knet") # Koç University deep learning framework implemented in Julia
 Pkg.add("KernelAbstractions") # Heterogeneous programming in Julia
+Pkg.add("LineSearches") # Line search methods for optimization and root-finding
 Pkg.add("LanguageServer")
 Pkg.add("LaTeXStrings")
 Pkg.add("LoopVectorization") # macro(s) for vectorizing loops
+Pkg.add("Lux") # Explicitly Parameterized Neural Networks in Julia
 Pkg.add("ModelingToolkit")
 #Pkg.add("NMFk") # Nonnegative Matrix Factorization + k-means clustering and physics constraints
 #Pkg.add("NTFk") # Nonnegative Tensor Factorization using k-means clustering and physics constraints
 #Pkg.add("NTNk") # Unsupervised Machine Learning: Nonnegative Tensor Networks + k-means clustering
 Pkg.add("ObjectDetector")
+Pkg.add("Optimization") # Mathematical Optimization in Julia. Local, global, gradient-based and derivative-free.
+Pkg.add("OptimizationOptimisers") # Optimization packages
+Pkg.add("OptimizationOptimJL")
+Pkg.add("OrdinaryDiffEq") # High performance ordinary differential equation (ODE) and differential-algebraic equation (DAE) solvers, including neural ordinary differential equations (neural ODEs) and scientific machine learning (SciML)
 Pkg.add("Plots")
 Pkg.add("Pluto")
 Pkg.add("PyCall")
 Pkg.add("PyPlot")
 Pkg.add("RCall")
+Pkg.add("StableRNGs") # A Julia RNG with stable streams
+Pkg.add("SciMLSensitivity") # A component of the DiffEq ecosystem for enabling sensitivity analysis for scientific machine learning (SciML). Optimize-then-discretize, discretize-then-optimize, adjoint methods, and more for ODEs, SDEs, DDEs, DAEs, etc.
 Pkg.add("Symbolics")
 Pkg.add("Sundials")
 Pkg.add("TextAnalysis")
