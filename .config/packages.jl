b7645d202eb9615af8d2a9a28cfe4482f0e48ba3
R. V. Lobato <rvlobato@pm.me>
2024-04-23 12:08:52 -0300

    Add some new packages for optimizations

--- .config/packages.jl
+++ .config/packages.jl
@@ -1,51 +1,55 @@
 import Pkg
-Pkg.add("AlphaZero") # Deepmind's AlphaZero algorithm
+#Pkg.add("AlphaZero") # Deepmind's AlphaZero algorithm
+#Pkg.add("CUDA") # CUDA programming in Julia
+#Pkg.add("NMFk") # Nonnegative Matrix Factorization + k-means clustering and physics constraints
+#Pkg.add("NTFk") # Nonnegative Tensor Factorization using k-means clustering and physics constraints
+#Pkg.add("NTNk") # Unsupervised Machine Learning: Nonnegative Tensor Networks + k-means clustering
+#Pkg.add("Turing")
 Pkg.add("Arrow") # implementation of the apache arrow memory
 Pkg.add("ArviZ") # Julia meta-package for exploratory analysis of Bayesian models
-Pkg.add("ComponentArrays") # Arrays with arbitrarily nested named components.
 Pkg.add("CSV") #A fast, flexible delimited file reader/writer for Julia.
-Pkg.add("CUDA") # CUDA programming in Julia
 Pkg.add("Clustering") # Methods for data clustering and evaluation of clustering quality
+Pkg.add("ComponentArrays") # Arrays with arbitrarily nested named components.
 Pkg.add("DataFrames") # Tools for working with tabular data in Julia.
 Pkg.add("DelimitedFiles")
 Pkg.add("Dierckx")
 Pkg.add("DiffEqGPU") # Massively Data-Parallel GPU Solving of ODEs
+Pkg.add("DiffEqParamEstim") # Easy scientific machine learning (SciML) parameter estimation with pre-built loss functions
 Pkg.add("DifferentialEquations")
 Pkg.add("Distributed")
 Pkg.add("Distributions")
 Pkg.add("Flux") # The Julia Machine Learning Library
-Pkg.add("Gadfly") # Gadfly is a plotting and data visualization system
+Pkg.add("ForwardDiff") # Forward Mode Automatic Differentiation for Julia
 Pkg.add("GLMakie") # High level plotting on the GPU.
+Pkg.add("Gadfly") # Gadfly is a plotting and data visualization system
 Pkg.add("IJulia") # Julia kernel for Jypyter
-Pkg.add("Knet") # Koç University deep learning framework implemented in Julia
 Pkg.add("KernelAbstractions") # Heterogeneous programming in Julia
-Pkg.add("LineSearches") # Line search methods for optimization and root-finding
-Pkg.add("LanguageServer")
+Pkg.add("Knet") # Koç University deep learning framework implemented in Julia
 Pkg.add("LaTeXStrings")
+Pkg.add("LanguageServer")
+Pkg.add("LineSearches") # Line search methods for optimization and root-finding
 Pkg.add("LoopVectorization") # macro(s) for vectorizing loops
 Pkg.add("Lux") # Explicitly Parameterized Neural Networks in Julia
 Pkg.add("ModelingToolkit")
-#Pkg.add("NMFk") # Nonnegative Matrix Factorization + k-means clustering and physics constraints
-#Pkg.add("NTFk") # Nonnegative Tensor Factorization using k-means clustering and physics constraints
-#Pkg.add("NTNk") # Unsupervised Machine Learning: Nonnegative Tensor Networks + k-means clustering
 Pkg.add("ObjectDetector")
 Pkg.add("Optimization") # Mathematical Optimization in Julia. Local, global, gradient-based and derivative-free.
-Pkg.add("OptimizationOptimisers") # Optimization packages
+Pkg.add("OptimizationBBO") # Mathematical Optimization in Julia. Local, global, gradient-based and derivative-free. Linear, Quadratic, Convex, Mixed-Integer, and Nonlinear Optimization in one simple, fast, and differentiable interface.
 Pkg.add("OptimizationOptimJL")
+Pkg.add("OptimizationOptimisers") # Optimization packages
 Pkg.add("OrdinaryDiffEq") # High performance ordinary differential equation (ODE) and differential-algebraic equation (DAE) solvers, including neural ordinary differential equations (neural ODEs) and scientific machine learning (SciML)
 Pkg.add("Plots")
 Pkg.add("Pluto")
 Pkg.add("PyCall")
 Pkg.add("PyPlot")
 Pkg.add("RCall")
-Pkg.add("StableRNGs") # A Julia RNG with stable streams
+Pkg.add("RecursiveArrayTools") # Tools for easily handling objects like arrays of arrays and deeper nestings in scientific machine learning (SciML) and other applications
 Pkg.add("SciMLSensitivity") # A component of the DiffEq ecosystem for enabling sensitivity analysis for scientific machine learning (SciML). Optimize-then-discretize, discretize-then-optimize, adjoint methods, and more for ODEs, SDEs, DDEs, DAEs, etc.
+Pkg.add("StableRNGs") # A Julia RNG with stable streams
 Pkg.add("StaticArrays") # Statically sized arrays for Julia
-Pkg.add("Symbolics")
 Pkg.add("Sundials")
+Pkg.add("Symbolics")
 Pkg.add("TextAnalysis")
-#Pkg.add("Turing")
-Pkg.add("VegaLite")
 Pkg.add("VegaDatasets")
+Pkg.add("VegaLite")
 Pkg.add("WaterLily") # Fast and simple fluid simulator in Julia
 Pkg.add("XGBoost")
