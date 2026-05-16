ec23e2006c14cef2a4ad03a45cd71627d03e770b
R. V. Lobato <rvlobato@pm.me>
2026-05-16 20:07:54 -0300

    refactored everything

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,14 @@
+;;; early-init.el --- Early startup -*- lexical-binding: t; -*-
+
+(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.6)
+
+(setq package-enable-at-startup nil)
+(setq frame-inhibit-implied-resize t)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-message t)
+(setq inhibit-startup-buffer-menu t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,56 +1,32 @@
-;;; Configs --- my emacs configs::
-;;; Commentary:
-;;; Code:
-;; Set customization data in a specific file, without littering init files.
-(setq custom-file "~/.emacs.d/custom.el")
-(ignore-errors (load custom-file))
+;;; init.el --- Minimal entry point -*- lexical-binding: t; -*-
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
 
 (let ((default-directory "/usr/share/emacs/site-lisp/"))
   (when (file-exists-p default-directory)
     (normal-top-level-add-subdirs-to-load-path)))
 
-;; Make all commands of the package module present.
 (require 'package)
 
-;; Internet repositories for new packages.
-(setq package-archives '(
-			 ("gnu" . "https://elpa.gnu.org/packages/")
-			 ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-			 ("melpa stable" . "https://stable.melpa.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 )
-      )
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa stable" . "https://stable.melpa.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
 
-;; Activate all the packages (in particular autoloads)
 (package-initialize)
-;; Fetch the list of packages available
+
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Pin third-party to MELPA Stable
-(setq use-package-always-pin "melpa stable")
-
-;;; Install my packages
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+
+(require 'org)
 (require 'use-package)
-(setq use-package-always-ensure t)
 
-;;; Auto package update
-(use-package auto-package-update
-  :defer 10
-  :config
-  ;; The periodicity (in days)
-  (setq auto-package-update-interval 30)
-  ;; ask before automatic update
-  (setq auto-package-update-prompt-before-update t)
-  ;; Delete residual old versions
-  (setq auto-package-update-delete-old-versions t)
-  ;; Do not bother me when updates have taken place.
-  (setq auto-package-update-hide-results t)
-  ;; Update installed packages at startup if there is an update pending.
-  (auto-package-update-maybe))
+(setq use-package-always-defer t)
 
-;;; Read my configs
-(org-babel-load-file "~/.emacs.d/configuration.org")
-;;; init.el ends here
+(org-babel-load-file
+ (expand-file-name "configuration.org" user-emacs-directory))
