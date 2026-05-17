eb367249e420f8f1a80c22ff3e6fdaae0a5a1701
R. V. Lobato <rvlobato@pm.me>
2026-05-16 22:26:10 -0300

    remove redundant configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,21 +12,18 @@
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa stable" . "https://stable.melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 
-(unless package-archive-contents
-  (package-refresh-contents))
-
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
+
 
 (require 'org)
 (require 'use-package)
-
-(setq use-package-always-defer t)
 
 (org-babel-load-file
  (expand-file-name "configuration.org" user-emacs-directory))
